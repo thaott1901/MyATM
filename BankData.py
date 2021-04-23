d0dd624649dfff6668c2bef0eec1b303ebde2578
ttran355 <ttran355@gatech.edu>
2021-04-23 17:45:19 -0400

    Simple ATM

---
+++ BankData.py
@@ -0,0 +1,92 @@
+customerList = {}
+
+
+def getCustomer(cardId):
+    if cardId in customerList:
+        return customerList.get(cardId)
+
+
+class Customer:
+    def __init__(self, cardID, pin):
+        self.cardID = cardID
+        self.pin = pin
+        self.account = {}
+        customerList[cardID] = self
+
+    def createAccount(self, accId, amt):
+        acc = Account(accId, amt)
+        self.account[accId] = acc
+
+    def getAccount(self, accId):
+        return self.account.get(accId)
+
+    def getAllAcounts(self):
+        return self.account.values()
+
+
+    def pinValid(self, input):
+        return input == self.pin
+
+    def getCardId(self):
+        return self.cardID
+
+    def getPin(self):
+        return self.pin
+
+class Account:
+    def __init__(self, accId, amt):
+        self.accId = accId
+        self.balance = amt
+        self.deposit = {}
+        self.withdraw = {}
+        self.timeSeed = 0
+
+    def depositCash(self, amount):
+        if amount > 0:
+            self.balance += amount
+            self.deposit[self.timeSeed] = amount
+            self.timeSeed += 1
+
+    def withdrawCash(self, amount):
+        if amount > 0:
+            self.balance -= amount
+            self.withdraw[self.timeSeed] = amount
+            self.timeSeed += 1
+
+    def getBalance(self):
+        return self.balance
+
+    def getDeposit(self):
+        return self.deposit
+
+    def getWithdraw(self):
+        return self.withdraw
+
+    def getAccId(self):
+        return self.accId
+
+
+
+# p1 = Customer("12345", "123")
+# p2 = Customer("54321", "543")
+# print(p1)
+# input = "1234"
+# print(p1.pinValid(input))
+#
+# print("Balance:", p1.balance)
+# print("Deposit:", p1.deposit)
+# print("Withdraw:", p1.withdraw)
+# print("-----------------------")
+# p1.depositCash(100)
+# print("Balance:", p1.balance)
+# print("Deposit:", p1.deposit)
+# print("Withdraw:", p1.withdraw)
+# print("-----------------------")
+# p1.withdrawCash(17)
+# print("Balance:", p1.balance)
+# print("Deposit:", p1.deposit)
+# print("Withdraw:", p1.withdraw)
+
+# print(customerList)
+# person1 = customerList.get(p1.cardID)
+# print(person1.pinValid("1234"))
---
+++ Main.py
@@ -0,0 +1,47 @@
+import BankData
+
+idValid = False
+count = 0
+
+cust1 = BankData.Customer("12345", "123")
+cust1.createAccount("a1", 12)
+cust1.createAccount("a2", 32)
+cust2 = BankData.Customer("54321", "543")
+cust2.createAccount("b1", 43)
+cust2.createAccount("b2", 15)
+
+cust1.getAccount("a1").depositCash(38)
+cust2.getAccount("b2").depositCash(22)
+
+cust1.getAccount("a2").withdrawCash(3)
+cust2.getAccount("b2").withdrawCash(7)
+
+print("Insert your card:")
+cardNum = input()
+cust = BankData.getCustomer(cardNum)
+
+accList = []
+print("Your card id is:", cust.getCardId())
+while not idValid:
+    print("Input your pin number:")
+    pinNum = input()
+    if pinNum == cust.getPin():
+        accList = cust.getAllAcounts()
+        idValid = True
+
+print(accList)
+print("Your list of current accounts:")
+for acc in accList:
+    print(acc.getAccId())
+
+accIdValid = False
+while not accIdValid:
+    print("Please select the account you want to check:")
+    accName = input()
+    for acc in accList:
+        if accName == acc.getAccId():
+            print("Account id: ", acc.getAccId())
+            print("Your account balance: ", acc.getBalance())
+            print("Your deposit history: ", acc.getDeposit())
+            print("Your withdraw history: ", acc.getWithdraw())
+            accIdValid = True
